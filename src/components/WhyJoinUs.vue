9dac6d53d77968ded3c4852d62812df23d9d0c82
Woodash <Woodash830@outlook.com>
2024-10-04 17:15:49 +0800

    feat: whyJoinUs

--- src/components/WhyJoinUs.vue
+++ src/components/WhyJoinUs.vue
@@ -1,13 +1,26 @@
 <script>
+import Card from './aboutUs/card.vue';
 import SectionTitle from './elements/SectionTItle.vue';
+
 export default {
     components: {
-        SectionTitle
+        SectionTitle,
+        Card
     }
 }
 </script>
 
 <template>
     <SectionTitle title="加入byrTeam的N个理由" />
-
+    <div class="flex mx-auto w-[96%] justify-between">
+        <Card title="开放自由" img="src/assets/whyJoinUs/1.svg">
+                自由开放的文化氛围让你的声音会被认真聆听，你可以与整个团队的同学进行思想碰撞，点燃创意的火花。在这里，你遇到的不只是学长学姐，更是并肩作战的伙伴。
+        </Card>
+        <Card title="收获满满" img="src/assets/whyJoinUs/2.svg">
+                无论你想提升自己的coding能力、产品思维能力还是交互设计能力，这里都有手握多个大厂offer的学长学姐手把手教学。从0到1带你做项目，成为你简历上最亮眼的一笔！
+        </Card>
+        <Card title="发挥价值" img="src/assets/whyJoinUs/3.svg">
+                这里有梦和追梦的勇气，我们为每一个有想法的同学搭建舞台，赋予把思考变为现实以满足用户需求的勇气和能力。好的内容，值得被更多人看见！
+        </Card>
+    </div>
 </template>
---
+++ src/components/aboutUs/card.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    props: ['img', 'title']
+}
+</script>
+
+<template>
+    <div class=" flex max-w-[344px] flex-col items-center">
+        <img :src="img" class="-mb-16 z-10">
+        <div class="flex flex-col gap-6 grow rounded-2xl bg-white text-center  px-12 pb-9 pt-[100px]">
+            <h2 class="text-[28px] text-center">
+                {{ title }}
+            </h2>
+            <span class="text-[#777] text-[0.96rem]">
+                <slot></slot>
+            </span>
+        </div>
+    </div>
+</template>
